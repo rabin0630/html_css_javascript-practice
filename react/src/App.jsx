a0fe00d3ce30cb2d6b78d1f43c42d42a2b18958f
SottoJunRabin <gunaid1231@outlook.jp>
2026-04-08 06:35:59 +0900

    reactの練習

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,38 @@
 import { useState } from 'react'
 import './index.css'
 import './App.css'
+import { Arrow,Bell,Bolt,Caret,Chevron,Cog,Messenger,Plus } from "./images";
 
 function App() {
-
-
   return (
     <Navbar>
-      <NavItem icon="☺️" />
-      <NavItem icon="☺️" />
-      <NavItem icon="☺️" />
+      <NavItem icon={ <Plus/>} />
+      <NavItem icon={ <Bell/>} />
+      <NavItem icon={<Messenger/>} />
+
+      <NavItem icon={<Caret/>}>
+      <DropdownMenu/>
+      </NavItem>
     </Navbar>
   );
+}
+
+function DropdownMenu(){
+  function DropdownItem(props) {
+    return(
+    <a href="#"className='menu-item'>
+      <span className='icon-button'>{props.leftIcon}</span>
+      {props.children}
+      <span className='icon-button'>{props.rightIcon}</span>
+    </a>
+    )
+  }
+  return(
+    <div className='dropdown'>
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem leftIcon={ <Cog></Cog> }rightIcon={ <Chevron></Chevron> }></DropdownItem>
+    </div>
+  )
 }
 
 function Navbar(props) {
@@ -23,11 +44,14 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+  const [open,setOpen] =useState(false);
   return (
     <li className='nav-item'>
-      <a href="" className='icon-button'>
+      <a href="#" className='icon-button' onClick={(e) => {e.preventDefault();setOpen(!open);}}>
         { props.icon }
       </a>
+
+      {open && props.children}
     </li>
   );
 }
---
+++ react/src/images/index.js
@@ -0,0 +1,10 @@
+import Arrow from "./arrow.svg?react";
+import Bell from "./bell.svg?react";
+import Bolt from "./bolt.svg?react";
+import Caret from "./caret.svg?react";
+import Chevron from "./chevron.svg?react";
+import Cog from "./cog.svg?react";
+import Messenger from "./messenger.svg?react";
+import Plus from "./plus.svg?react";
+
+export{ Arrow,Bell,Bolt,Caret,Chevron,Cog,Messenger,Plus };
